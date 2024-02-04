19025ef78a163e41fd53a88ab63b0d2777af6955
Zil456 <Zilvinas_baltinas@yahoo.com>
2024-02-04 22:09:31 +0000

    tic tac toe completed

--- src/App.js
+++ src/App.js
@@ -14,27 +14,39 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
+  // const [xIsNext, setXIsNext] = useState(true);
 
-  const [squares, setSquares] = useState(Array(9).fill(null)); // Array(9).fill(null) creates an array of 9 nulls. This is the initial state of the squares array
-  // const [squares, setSquares] = useState([null, null, null, null, null, null, null, null, null]); // This is the same as above
-  // const [squares, setSquares] = useState(Array(9).fill('X')); // This is the same as above but with Xs instead of nulls
+  // const [squares, setSquares] = useState(Array(9).fill(null)); // Array(9).fill(null) creates an array of 9 nulls. This is the initial state of the squares array
+  // // const [squares, setSquares] = useState([null, null, null, null, null, null, null, null, null]); // This is the same as above
+  // // const [squares, setSquares] = useState(Array(9).fill('X')); // This is the same as above but with Xs instead of nulls
 
   function handleClick(i) {
-    if (squares[i]) {
-      // if the square already has a value, return early. This prevents a square from being changed after it has already been clicked
-      // a simpler way to write this is: if (squares[i] !== null)
+    if (calculateWinner(squares) || squares[i]) {
+      // in simple terms, here we check if the square has a value or if there is a winner
+      // if the square has a value or if there is a winner, we return and do nothing
+
       return;
     }
+
     const nextSquares = squares.slice(); // squares.slice() creates a copy of the squares array to modify instead of modifying the existing array
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+    //   setSquares(nextSquares);
+    //   setXIsNext(!xIsNext);
+    // }
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
@@ -43,6 +55,7 @@ export default function Board() {
     // The handleClick function then creates a copy of the squares array and modifies the copy instead of the original array.
 
     <>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -62,6 +75,51 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -75,7 +133,7 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]; // here we use destructuring to assign values from the lines array to variables a, b, and c. This is the same as writing const a = lines[i][0]; const b = lines[i][1]; const c = lines[i][2];
-    if (squares[a] && (squares[a] === squares[b]) === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       // here we check if the squares array has a value at index a and if the value at index a is equal to the value at index b and if the value at index a is equal to the value at index c
       // if the above condition is true, we return the value at index a. This means that if the value at index a is equal to the value at index b and the value at index c, we return the value at index a
       // if the value at index a is not equal to the value at index b and the value at index c, we return null
